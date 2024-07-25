b47cdc05d570f9e06e5bba4d771d0e0ffd6956b8
hamxasajid <hamzasajid4341@gmail.com>
2024-07-25 14:19:44 +0500

    Bootstrap Frontend Bootcamp

--- DigitalResturant/Js/myscript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.toggle-button');
-    const navButton = document.querySelector('.button');
-
-    toggleButton.addEventListener('click', function () {
-        navButton.classList.toggle('active');
-    });
-});
--- README.md
+++
@@ -1,4 +0,0 @@
-# DIGITAL RESTAURANT
-
-| Digital Restaurant   | [View Demo](https://hamxasajid.github.io/LandingPage/DigitalResturant/index.html) |
-|----------------------|-----------|
